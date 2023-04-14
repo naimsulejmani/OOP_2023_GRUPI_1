613bcfff8a10e5e230d6d66f4cf76e40da09568a
Naim Sulejmani <naim.sulejmani@radix.com>
2023-04-14 16:03:52 +0200

    Inheritance + COmposition

--- src/inheritance/payroll_app/PayrollGenerator.java
+++ src/inheritance/payroll_app/PayrollGenerator.java
@@ -9,23 +9,59 @@ public class PayrollGenerator {
                 "3,Aldi,Krasniqi,consulent,2022,1,90,20;";
 
         ArrayList<Employee> employees = new ArrayList<>();
+        ArrayList<Paycheck> paychecks = new ArrayList<>();
 
         String[] empString = fileText.split(";");
 
-        for (String employeeStr : empString){
+        for (String employeeStr : empString) {
             String[] attributes = employeeStr.split(",");
             int id = Integer.parseInt(attributes[0]);
             String name = attributes[1];
             String surname = attributes[2];
             String title = attributes[3].toLowerCase();
-
+            int year = Integer.parseInt(attributes[4]);
+            int month = Integer.parseInt(attributes[5]);
 
             switch (title) {
+                case "programer", "manager" -> {
+                    float salary = Float.parseFloat(attributes[6]);
+                    float bonus = Float.parseFloat(attributes[7]);
 
+                    Employee e = title.equals("programer") ?
+                            new Programmer(id, name, surname, salary, bonus) :
+                            new Manager(id, name, surname, salary, bonus);
+
+                    paychecks.add(new Paycheck(month, year, e));
+                }
+                case "consulent" -> {
+                    int hours = Integer.parseInt(attributes[6]);
+                    float hourlyWage = Float.parseFloat(attributes[7]);
+
+                    Employee e = new Consulent(id, name, surname, hours, hourlyWage);
+
+                    employees.add(e);
+
+                    paychecks.add(new Paycheck(month, year, e));
+                }
             }
         }
+
+        for (Paycheck paycheck : paychecks) {
+            //email send ose printer print ne jeten reale
+            System.out.println(paycheck);
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 
